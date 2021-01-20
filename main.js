575841dc286e65f73f922c1fa166422632812303
TaylorHardaway <gthardaway@gmail.com>
2021-01-19 19:45:51 -0600

    array notes

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Making notes for arrays and loops")
+const students = ['Katy', 'Jackie', 'Sean', 'Nathan'];
+//console.log (students.length)
+
+//**************accessing items in an array */
+//console.log(students[1]);
+//console.log(students[students.length - 1]); //Nathan
+
+students[0] = 'Matthew';
+//console.log(students);
+
+const valuePrinter = (array, index) => {
+  return (array[index]);
+}
+
+console.log(valuePrinter[1,2,3,4,5,6], 4);
+
+const checkStudent = (array, students) => {
+  return array.includes(students)
+  
+}
+console.log(checkStudent(students, 'Greg'));
